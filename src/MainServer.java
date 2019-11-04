d59abd03cef084d281de727d0380382e3438d495
Andy <aw087ok@gmail.com>
2019-11-04 09:59:29 +0000

    GUI console screen for server

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,10 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class MainServer {
 
+	// Swing GUI management
 	private JFrame frame;
+	private JTextArea consoleScreen;
 
 	/**
 	 * Launch the application.
@@ -28,14 +31,43 @@ public class MainServer {
 	public MainServer() {
 		initialize();
 	}
+	
+	/**
+	 * Sets up the main GUI console screen for the server.
+	 */
+	private void initializeConsoleScreen() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		consoleScreen = new JTextArea();
+		consoleScreen.setEditable(false);
+		consoleScreen.setWrapStyleWord(true);
+		consoleScreen.setBounds(10, 10, 415, 240);
+		frame.getContentPane().add(consoleScreen);
+	}
+	
+	/**
+	 * Sets up JDBC connection to the locally hosted SQL database.
+	 */
+	private void connectToSQLDatabase() {
+		
+	}
+	
+	/**
+	 * Starts the server for handling user requests.
+	 */
+	private void initializeServer() {
+		
+	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initializeConsoleScreen();
+		connectToSQLDatabase();
+		initializeServer();
 	}
-
 }
