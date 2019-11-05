e1847b5b0ef29e34d4fcddd4d21d396f70894a2a
Andy <aw087ok@gmail.com>
2019-11-05 21:46:36 +0000

    Database backend functions for handling user login, and students retrieval

--- src/DatabaseController.java
+++ src/DatabaseController.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DatabaseController {
@@ -12,6 +15,10 @@ public class DatabaseController {
 	private final String DATABASE_NAME = "test";
 	private final String[] TABLE_NAMES = { "users", "students" };
 	private Connection sql; // Main connection variable to the database
+	
+	public DatabaseController() {
+		connectToSQLDatabase();
+	}
 	
 	/**
 	 * Sets up JDBC connection to the locally hosted SQL database.
@@ -33,5 +40,78 @@ public class DatabaseController {
 			// consoleScreen.append("Connected to SQL database " + DATABASE_NAME + ".\n");
 		} catch (Exception e) {
 		}
+	}
+	
+	/**
+	 * Returns a boolean according to the given UNAME and if it is a valid username from the `users` table.
+	 * 
+	 * @param uname
+	 * @return
+	 */
+	public boolean userLogin(String uname) {
+		try {
+			Statement stmt = this.sql.createStatement();
+			
+			stmt.executeQuery(
+					"SELECT * FROM `users` " +
+					"WHERE (UNAME = " + uname + ")"
+			);
+			
+			ResultSet found = stmt.getResultSet();
+			
+			// found.first gives true if value exists
+			boolean loggedIn = found.first();
+			found.close();
+			
+			return loggedIn;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Returns a ResultSet corresponding to all the students in the `students` table
+	 * 
+	 * @return
+	 */
+	public ResultSet getAllStudentRecords() {
+		try {
+			Statement stmt = this.sql.createStatement();
+			
+			stmt.executeQuery(
+					"SELECT * FROM `students`"
+			);
+			
+			return stmt.getResultSet();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns a ResultSet corresponding to the list of students in the `students table` with the given surname (SNAME)
+	 * 
+	 * @param sname
+	 * @return
+	 */
+	public ResultSet searchStudentBySurname(String sname) {
+		try {
+			Statement stmt = this.sql.createStatement();
+			
+			stmt.executeQuery(
+					"SELECT * FROM `students` " +
+					"WHERE (SNAME = " + sname + ")"
+			);
+			
+			return stmt.getResultSet();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
