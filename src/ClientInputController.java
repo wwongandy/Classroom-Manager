1ad25ecbfd29b9c52e2d6c828ec883666bea4bdf
Andy <aw087ok@gmail.com>
2019-11-06 00:03:04 +0000

    Client input controller now stores a single socket and display handler only

--- src/ClientInputController.java
+++ src/ClientInputController.java
@@ -1,15 +1,35 @@
 import java.net.InetAddress;
 import java.net.Socket;
 
+import javax.swing.JTextArea;
+
 public class ClientInputController extends Thread {
 
-	private InetAddress ipAddress;
+	private Socket socket;
+	private JTextArea consoleScreen;
 	
-	public ClientInputController(Socket incomingSocket) {
-		ipAddress = incomingSocket.getInetAddress();
+	public ClientInputController(Socket incomingSocket, JTextArea consoleScreen) {
+		this.setSocket(incomingSocket);
+		this.setConsoleScreen(consoleScreen);
 	}
 	
 	public void run() {
 		
+	}
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+	public JTextArea getConsoleScreen() {
+		return consoleScreen;
+	}
+
+	public void setConsoleScreen(JTextArea consoleScreen) {
+		this.consoleScreen = consoleScreen;
 	}
 }
